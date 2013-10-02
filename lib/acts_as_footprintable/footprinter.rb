43673d79d879598bbf45fe063ecc5c9eabab0771
patorash <chariderpato@gmail.com>
2013-10-02 15:08:59 +0900

    モデル毎にユーザのアクセス履歴を取得する機能を追加した。

--- lib/acts_as_footprintable/footprinter.rb
+++ lib/acts_as_footprintable/footprinter.rb
@@ -3,16 +3,24 @@ module ActsAsFootprintable
   module Footprinter
     def self.included(base)
       base.class_eval do
-        has_many :footprints, :class_name => 'ActsAsFootprint::Footprint', :as => :footprinter, :dependent => :destroy do
+        has_many :footprints, :class_name => 'ActsAsFootprintable::Footprint', :as => :footprinter, :dependent => :destroy do
           def footprintable
             includes(:footprintable).map(&:footprintable)
           end
         end
       end
     end
-  end
 
-  def footprint(args)
-    args[:footprintable].footprint args.merge({:footprinter => self})
+    def footprint(args)
+      args[:footprintable].footprint args.merge({:footprinter => self})
+    end
+
+    def access_histories_for(klass, limit=nil)
+      records = footprints.for_type(klass).
+          includes(klass.table_name.singularize).
+          order("footprints.created_at desc").select("DISTINCT footprintable_id, footprintable_type")
+      records = records.limit(limit) unless limit.nil?
+      records.map{|footprint| footprint.footprintable}
+    end
   end
 end
--- spec/footprinter_spec.rb
+++ spec/footprinter_spec.rb
@@ -10,4 +10,29 @@ describe ActsAsFootprintable::Footprinter do
   it "should be a footprinter" do
     User.should be_footprinter
   end
+
+  describe "ユーザーのアクセス履歴を" do
+    before do
+      @user = User.create!(:name => "user")
+      (1..5).each do |index|
+        footprintable = Footprintable.create!(:name => "footprintable#{index}")
+        3.times { footprintable.leave_footprints @user }
+      end
+    end
+
+    context "対象のモデル毎に" do
+      it "取得できること" do
+        @user.access_histories_for(Footprintable).should have(5).items
+        @user.access_histories_for(Footprintable).map{|footprintable| footprintable.name}.should == (1..5).to_a.reverse.map{|index| "footprintable#{index}"}
+      end
+
+      it "件数を絞り込めること" do
+        @user.access_histories_for(Footprintable, 3).should have(3).items
+      end
+    end
+
+    context "全てのモデルを通じて" do
+      it "取得できること"
+    end
+  end
 end
